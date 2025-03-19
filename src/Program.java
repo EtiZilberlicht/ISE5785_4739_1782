1a5e26b6774c336c50442cb13afc2412a6baef63
Eti Zilberlicht <ezilberl@g.jct.ac.il>
2025-03-19 12:49:50 +0200

    Second Commit

--- src/Program.java
+++ src/Program.java
@@ -3,6 +3,8 @@ public class Program {
 
 	public static void main(String[] args) {
         System.out.println("------------------------");
+        System.out.println("------------------------");
+
 	}
 
 }
